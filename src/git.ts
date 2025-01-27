4aaff29164372f4443c79afd6c6bcf6a9ed18273
Ygor Azambuja <ygorazambuja@gmail.com>
2025-01-27 17:07:26 -0400

    feat: ajusta função git

--- src/git.ts
+++ src/git.ts
@@ -31,8 +31,6 @@ export async function getDiff(gitFolder: string) {
   });
 
   for (const c of corrects) {
-    console.log(c.value?.fullFilePath);
-
     const { stderr, exitCode } = await $`git add ${c.value?.fullFilePath}`
       .nothrow()
       .quiet();
@@ -44,19 +42,32 @@ export async function getDiff(gitFolder: string) {
 
   return jsons;
 }
-
 async function getAlteredFileNames(gitFolder: string): Promise<Array<string>> {
-  const proc = Bun.spawn(["git", "diff", "--name-only"]);
+  const diffProc = Bun.spawn(["git", "diff", "--name-only"]);
+  const newFilesProc = Bun.spawn([
+    "git",
+    "ls-files",
+    "--others",
+    "--exclude-standard",
+  ]);
 
   const ignoredPatterns = ["pnpm-lock.yaml", "bun.lockb", "package-lock.json"];
 
-  const text = await new Response(proc.stdout).text();
-  const files = text
+  const diffText = await new Response(diffProc.stdout).text();
+  const diffFiles = diffText
     .split("\n")
     .filter(Boolean)
     .filter((f) => !ignoredPatterns.includes(f))
     .map((f) => gitFolder.concat(`/${f}`))
     .map((f) => f.replaceAll("\n", ""));
 
-  return files;
+  const newFilesText = await new Response(newFilesProc.stdout).text();
+  const newFiles = newFilesText
+    .split("\n")
+    .filter(Boolean)
+    .filter((f) => !ignoredPatterns.includes(f))
+    .map((f) => gitFolder.concat(`/${f}`))
+    .map((f) => f.replaceAll("\n", ""));
+
+  return [...diffFiles, ...newFiles];
 }
